c78e4fbf1c9391088b6ae26b94126006504f8739
hamadayuuki <yuukirinnma@gmail.com>
2024-01-05 22:10:52 +0900

    Fix db for migrate

--- rest-api/db/db.go
+++ rest-api/db/db.go
@@ -2,10 +2,12 @@ package db
 
 // TODO: $ go mod tidy
 import (
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -19,14 +21,7 @@ func NewDB() *gorm.DB {
 	}
 
 	// URL をもとにDBへ接続
-	url := fmt.Sprintf(
-		"postgres://%s:%s@%s:%s/%s",
-		os.Getenv("POSTGRES_USER"),
-		os.Getenv("POSTGRES_PW"),
-		os.Getenv("POSTGRES_HOST"),
-		os.Getenv("POSTGRES_PORT"),
-		os.Getenv("POSTGRES_DB")
-	)
+	url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PW"), os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_DB"))
 	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
 
 	if err != nil { log.Fatalln(err) }
