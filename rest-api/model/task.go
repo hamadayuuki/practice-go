e4e813f376b3235d5f619dc5370ef08f8bb0eab9
hamadayuuki <yuukirinnma@gmail.com>
2024-01-05 21:08:39 +0900

    Update task

--- rest-api/model/task.go
+++ rest-api/model/task.go
@@ -1,41 +1,19 @@
-package db
+package model
 
-// TODO: $ go mod tidy
-import (
-	"log"
-	"os"
+import "time"
 
-	"github.com/joho/godotenv"
-	"gorm.io/gorm"
-)
-
-// DB への接続
-func NewDB() *gorm.DB {
-	if os.Getenv("GO_ENV") == "dev" {
-		err := godotenv.Load()
-		if err != nil {
-			log.Fatalln(err)
-		}
-	}
-
-	// URL をもとにDBへ接続
-	url := fmt.Sprintf(
-		"postgres://%s:%s@%s:%s/%s",
-		os.Getenv("POSTGRES_USER"),
-		os.Getenv("POSTGRES_PW"),
-		os.Getenv("POSTGRES_HOST"),
-		os.Getenv("POSTGRES_PORT"),
-		os.Getenv("POSTGRES_DB")
-	)
-	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
-
-	if err != nil { log.Fatalln(err) }
-	fmt.Println("Connected")
-	return db
+type Task struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Title     string    `json:"title" gorm:"not null"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	User      User      `json:"user" gorm:"foreignKey:UserId; constraint:OnDelete:CASCADE"`
+	UserId    uint      `json:"user_id" gorm:"not null"`
 }
 
-// DB との接続解除
-func CloseDB(db *gorm.DB) {
-	sqlDB, _ := db.DB()
-	if err := sqlDB.Close(); err != nil { log.Fatalln(err) }
+type TaskResponse struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Title     string    `json:"title" gorm:"not null"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
