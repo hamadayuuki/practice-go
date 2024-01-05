91d9eabd4956ff126b89853e760503fd4c028110
hamadayuuki <yuukirinnma@gmail.com>
2024-01-05 23:38:12 +0900

    Update usecase for impl

--- rest-api/go.mod
+++ rest-api/go.mod
@@ -3,7 +3,9 @@ module go-rest-api
 go 1.21.5
 
 require (
+	github.com/golang-jwt/jwt/v4 v4.5.0
 	github.com/joho/godotenv v1.5.1
+	golang.org/x/crypto v0.14.0
 	gorm.io/driver/postgres v1.5.4
 	gorm.io/gorm v1.25.5
 )
@@ -14,6 +16,5 @@ require (
 	github.com/jackc/pgx/v5 v5.4.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 )
--- rest-api/usecase/user_usecase.go
+++ rest-api/usecase/user_usecase.go
@@ -1,9 +1,67 @@
 package usecase
 
-import "go-rest-api/model"
+import (
+	"go-rest-api/model"
+	"go-rest-api/repository"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+	"golang.org/x/crypto/bcrypt"
+)
 
 type IUserUseCase interface {
 	SignUp(user model.User) (model.UserResponse, error)
 	// JWT token を返すため、string を返すようにしている
 	Login(user model.User) (string, error)
+}
+
+// MARK: - UseCase の実体
+
+type userUsecase struct {
+	userRepository repository.IUserRepository
+}
+
+func NewUserUsecase(userRepository repository.IUserRepository) IUserUsecase {
+	return &userUsecase{userRepository}
+}
+
+func (userUsecase *userUsecase) SignUp(user model.User) (model.UserResponse, error) {
+	// パスワードをハッシュ化
+	hashedPassword, error := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
+	if err != nil { return model.UserResponse{}, err }
+
+	// DB へ書きこむUserを作成
+	newUser := model.User{Email: user.Email, Password: string(hashedPassword)}
+	if err := userUsecase.userRepository.CreateUser(&newUser); err != nil { return model.UserResponse{}, err }   // TODO: CreateUser() は今後実装する
+
+	// repository? へのレスポンス用のUserを作成
+	resUser := model.UserResponse{
+		ID:    newUser.ID,
+		Email: newUser.Email
+	}
+	return resUser, nil
+}
+
+func (userUsecase *userUsecase) Login(user model.User) (string, error) {
+	// storedUser は初期化しているだけではないのか？ データが格納されているのか？ されていない場合CompareHashAndPassword() で何を比較しているのか
+	storedUser := model.User{}   // 初期化
+	if err := userUsecase.userRepository.GetUserByEmail(&storedUser, user.Email); err != nil {   // TODO: GetUserByEmail() は今後実装する
+		return "", err
+	}
+
+	// パスワード の確認
+	err := bcrypt.CompareHashAndPassword([]byte(storedUser.Password), []byte(user.Password))
+	if err != nil { return "", err }
+
+	// JWT を発行
+	// JWT : JSON をやり取りする際のデジタル認証情報
+	toke := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims {
+		"user_id": storedUser.ID,
+		"exp"    : time.Now().Add(time.Hour * 12).Unix(),
+	})
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET")))   // SECRET=uu5pveql
+	if err != nil { return "", err }
+
+	return tokenString, nil
 }
