8e656a3c7473123a11302e451a2e4592cef4a574
hamadayuuki <yuukirinnma@gmail.com>
2024-01-05 23:49:25 +0900

    Update user repository for impl

--- rest-api/repository/user_repository.go
+++ rest-api/repository/user_repository.go
@@ -1,9 +1,38 @@
 package repository
 
-import "go-rest-api/model"
+import (
+	"go-rest-api/model"
+
+	"gorm.io/gorm"
+)
 
 type IUserRepository interface {
 //                 引数                            戻り値
 	GetUserByEmail(user *model.User, email string) error
 	CerateUser(user *model.User) error
+}
+
+// MARK: - User Repository の実体
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+// DBからのDI用
+func NewUserRepository(db *gorm.DB) IUserRepository {
+	return &userRepository(db)
+}
+
+func (userRepository *userRepository) GetUserByEmail(user *model.User, email string) error {
+	if err := userRepository.db.Where("email=?", email).First(user).Error; err != nil {   // TODO: Where() は今後実装
+		return err
+	}
+	return nil
+}
+
+func (userRepository *userRepository) CerateUser(user *model.User) error {
+	if err := userRepository.db.Create(user).Error; err != nil {   // TODO: Create() は今後実装
+		return err
+	}
+	return nil
 }
