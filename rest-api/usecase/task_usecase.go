e03237ad8a15b42aeafaf7273ea357af722e213a
hamadayuuki <yuukirinnma@gmail.com>
2024-01-13 01:28:49 +0900

    Add interface of task usecase

---
+++ rest-api/usecase/task_usecase.go
@@ -0,0 +1,11 @@
+import (
+	"go-rest-api/model"
+)
+
+type ITaskUsecase interface {
+	GetAllTasks(userId uint) ([]model.TaskResponse, error)
+	GetTaskById(userId uint, taskId uint) (model.TaskResponse, error)
+	CreateTask(task model.Task) (model.TaskResponse, error)
+	UpdateTask(task model.Task, userId uint, taskId uint) (model.TaskResponse, error)
+	DeleteTask(userId uint, taskId uint) error
+}
