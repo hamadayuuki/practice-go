badf5692a9089735330e992740cd64958b13050a
hamadayuuki <yuukirinnma@gmail.com>
2024-01-18 14:59:57 +0900

    Add interface of task controller

---
+++ rest-api/controller/task_controller.go
@@ -0,0 +1,15 @@
+package controller
+
+import (
+	"go-rest-api/model"
+	
+	"github.com/labstack/echo/v4"
+)
+
+type ITaskController interface {
+	GetAllTasks(c echo.Context) error
+	GetTaskById(c echo.Context) error
+	CreateTask(c echo.Context) error
+	UpdateTask(c echo.Context) error
+	DeleteTask(c echo.Context) error
+}
