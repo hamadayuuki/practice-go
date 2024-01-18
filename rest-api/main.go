7ac4d1ddb3d3f26a35e082e816f83d4ffc601631
hamadayuuki <yuukirinnma@gmail.com>
2024-01-18 15:33:45 +0900

    Update main for connect task repository-usecase-contorller

--- rest-api/main.go
+++ rest-api/main.go
@@ -12,8 +12,11 @@ func main() {
 	// クリーンアーキテクチャの外から内に注入していく
 	db := db.NewDB()
 	userRepository := repository.NewUserRepository(db)
+	taskRepository := repository.NewTaskRepository(db)
 	userUsecase := usecase.NewUserUsecase(userRepository)
+	taskUsecase := usecase.NewTaskUsecase(taskRepository)
 	userController := controller.NewUserController(userUsecase)
-	e := router.NewRouter(userController)
+	taskController := controller.NewTaskController(taskUsecase)
+	e := router.NewRouter(userController, taskController)
 	e.Logger.Fatal(e.Start(":8080"))
 }
