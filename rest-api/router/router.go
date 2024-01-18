68de5e05c2545ca90da5774ae203e4a897f806c5
hamadayuuki <yuukirinnma@gmail.com>
2024-01-18 15:31:06 +0900

    Update router for task

--- rest-api/router/router.go
+++ rest-api/router/router.go
@@ -6,10 +6,18 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func NewRouter(uc controller.IUserController) *echo.Echo {
+func NewRouter(uc controller.IUserController, tc controller.ITaskController) *echo.Echo {
 	e := echo.New()
 	e.POST("/signup", uc.SignUp)
 	e.POST("/login", uc.LogIn)
 	e.POST("/logout", uc.LogOut)
+
+	t := e.Group("/tasks")
+	t.GET("", tc.GetAllTasks)
+	t.GET("/:taskId", tc.GetTaskById)
+	t.POST("", tc.CreateTask)
+	t.PUT("/:taskId", tc.UpdateTask)
+	t.DELETE("/:taskId", tc.DeleteTask)
+	
 	return e
 }
