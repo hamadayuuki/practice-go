99d961a92c200838947f53305d20c5eec230cc8c
hamadayuuki <yuukirinnma@gmail.com>
2024-01-22 20:18:14 +0900

    Update comment for task validation impl

--- rest-api/validator/task_validator.go
+++ rest-api/validator/task_validator.go
@@ -10,6 +10,20 @@ type ITaskValidator interface {
 	TaskValidate(task model.task) error
 }
 
+// MARK: - task validator の実体
 
+type taskValidator struct{}
 
+func NewTaskValidator() ITaskValidator {
+	return &taskValidator{}
+}
 
+func (tv *taskValidator) TaskValidate(task model.task) error {
+	return validation.ValidateStruct(&task,
+		validation.Field(
+			&task.Title,
+			validation.Required.Error("title is required"),   // 値が格納されているか
+			validation.RuneLength(1, 10).Error("limited max 10 char"),   // 文字の長さが1〜10か
+		),
+	)
+}
