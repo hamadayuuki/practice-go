4d926a2dc487841f59fffcf02a260bf8681d3003
hamadayuuki <yuukirinnma@gmail.com>
2024-01-24 19:40:26 +0900

    Add CORS to router

--- rest-api/router/router.go
+++ rest-api/router/router.go
@@ -6,10 +6,23 @@ import (
 
 	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func NewRouter(uc controller.IUserController, tc controller.ITaskController) *echo.Echo {
 	e := echo.New()
+
+	// CORS
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig {
+		AllowOrigins: []string { "http://localhost:3000", os.Getenv("FE_URL") },
+		AllowHeaders: []string { 
+			echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept,
+			echo.HeaderAccessControlAllowHeaders, echo.HeaderXCRFToken 
+		},
+		AllowMethods: []string { "GET", "PUT", "POST", "DELETE" },
+		AllowCredentials: true,
+	}))
+
 	e.POST("/signup", uc.SignUp)
 	e.POST("/login", uc.LogIn)
 	e.POST("/logout", uc.LogOut)
