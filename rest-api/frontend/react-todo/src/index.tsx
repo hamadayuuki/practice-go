c8e13a7618a7056c3097c1d2aa2057510f13ebd9
hamadayuuki <yuukirinnma@gmail.com>
2024-01-28 14:39:50 +0900

    Update index.tsx for use react-query+devtools

--- rest-api/frontend/react-todo/src/index.tsx
+++ rest-api/frontend/react-todo/src/index.tsx
@@ -4,12 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'   // データフェッチングやサーバー状態の管理を行う
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'   // デバッグ
+
+
+const queryClient = new QueryClient({})
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <QueryClientProvider client = {queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen = {false} />
+      </QueryClientProvider>
   </React.StrictMode>
 );
 
