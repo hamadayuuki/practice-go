818790fef32d04d2a71c53770f8915a91e8d6dad
hamadayuuki <yuukirinnma@gmail.com>
2024-01-28 16:03:49 +0900

    Update App.tsx for request to API

--- rest-api/frontend/react-todo/src/index.tsx
+++ rest-api/frontend/react-todo/src/index.tsx
@@ -26,3 +26,7 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// for Fix error
+// error: 'index.tsx' cannot be compiled under '--isolatedModules' because it is considered a global script file. Add an import, export, or an empty 'export {}' statement to make it a module.
+export {}
