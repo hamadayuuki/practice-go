36a5201720958181799cd9d3a04f4dbd340e564d
hamadayuuki <yuukirinnma@gmail.com>
2024-01-30 11:47:34 +0900

    Update UI of auth component

--- rest-api/frontend/react-todo/src/components/Auth.tsx
+++ rest-api/frontend/react-todo/src/components/Auth.tsx
@@ -10,7 +10,7 @@ export const Auth = () => {
     const [isLogin, setIsLogin] = useState(true)
     const { loginMutation, registerMutation } = useMutateAuth()
 
-    const submitAuthHadler = async (e: FormEvent<HTMLFormElement>) => {
+    const submitAuthHandler = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
 
         if (isLogin) {
@@ -33,5 +33,51 @@ export const Auth = () => {
         }
     }
 
-    return <div>Auth</div>
+    return (
+        <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 font-mono">
+          <div className="flex items-center">
+            <CheckBadgeIcon className="h-8 w-8 mr-2 text-blue-500" />
+            <span className="text-center text-3xl font-extrabold">
+              Todo app by React/Go(Echo)
+            </span>
+          </div>
+          <h2 className="my-6">{isLogin ? 'Login' : 'Create a new account'}</h2>
+          <form onSubmit={submitAuthHandler}>
+            <div>
+              <input
+                className="mb-3 px-3 text-sm py-2 border border-gray-300"
+                name="email"
+                type="email"
+                autoFocus
+                placeholder="Email address"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+              />
+            </div>
+            <div>
+              <input
+                className="mb-3 px-3 text-sm py-2 border border-gray-300"
+                name="password"
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPw(e.target.value)}
+                value={pw}
+              />
+            </div>
+            <div className="flex justify-center my-2">
+              <button
+                className="disabled:opacity-40 py-2 px-4 rounded text-white bg-indigo-600"
+                disabled={!email || !pw}
+                type="submit"
+              >
+                {isLogin ? 'Login' : 'Sign Up'}
+              </button>
+            </div>
+          </form>
+          <ArrowPathIcon
+            onClick={() => setIsLogin(!isLogin)}
+            className="h-6 w-6 my-2 text-blue-500 cursor-pointer"
+          />
+        </div>
+      )
 }
